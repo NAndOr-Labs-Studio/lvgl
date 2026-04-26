90a6a856ac61494c40334613922125a3888e15c9
physics1024 <15101215652@163.com>
2026-04-27 02:57:31 +0800

    adapt to Pico

---
+++ lv_conf.h
@@ -0,0 +1,21 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#include <stdint.h>
+
+#define LV_COLOR_DEPTH 16
+
+#define LV_USE_DRAW_SW 1
+
+#define LV_USE_THORVG_INTERNAL 0
+#define LV_USE_THORVG_EXTERNAL 0
+
+#define LV_BUILD_EXAMPLES 0
+#define LV_BUILD_DEMOS 0
+
+#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_DEFAULT &lv_font_montserrat_14
+
+#define LV_USE_LOG 0
+
+#endif
